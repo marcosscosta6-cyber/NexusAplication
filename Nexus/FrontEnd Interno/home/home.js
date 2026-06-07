328d7fac6addbcc01fad6d8fb757534c86bc29ab
Ana Tex <ana.carvalhoteixeira@ba.estudante.senai.br>
2026-06-07 19:05:00 -0300

    funcionamento de alguns links e botões do home

---
+++ Nexus/FrontEnd Interno/home/home.js
@@ -0,0 +1,8 @@
+// Adiciona um evento de clique ao botão "Ver todas as notificações"
+document.getElementById("verAtvds").addEventListener("click", function() {
+    window.location.href = "../notificações/not.html";
+});
+
+document.getElementById("explorarBtn").addEventListener("click", function() {
+    window.location.href = "../explorar/explorar.html";
+});
